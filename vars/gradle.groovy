d507b93d701999ab64b3ababbcc19887897355c9
Luis Garrido <garrluis@gmail.com>
2021-01-05 17:34:46 -0300

    modificacion

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -19,21 +19,22 @@ def call(String miparam){
         build.call()
         // sonar.call()
 
-        stage('Sonar') 
-        {
+    stage('Sonar') 
+	{
+        env.TAREA =  env.STAGE_NAME
+		stage('SonarQube analysis') 
+		{
             env.TAREA =  env.STAGE_NAME
-            stage('SonarQube analysis') 
-            {
-                env.TAREA =  env.STAGE_NAME
-                
-                def scannerHome = tool 'Sonar-Scanner';
+		    // Coresponde a lo que se configuro en tool conffiguration
+			def scannerHome = tool 'Sonar-Scanner';
 
-                withSonarQubeEnv('Sonar-Server') 
-                { 
-                    bat start "${scannerHome}\\bin\\Sonar-Scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
-                }
-            }
-        }
+			withSonarQubeEnv('Sonar-Server') 
+			{ 
+                bat start "${scannerHome}\\bin\\Sonar-Scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+			}
+		}
+    }
+
 
         run.call()
         rest.call()
