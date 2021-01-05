94b820383838a47d66e63c06e6c32a095a553bc0
Luis Garrido <garrluis@gmail.com>
2021-01-05 18:16:55 -0300

    modificacion

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -17,27 +17,7 @@ def call(String miparam){
     if (miparam == ";") {
         println "El valor ingresado es vacio, se procesan todos los stages: " 
         build.call()
-        // sonar.call()
-        sleep(10) {
-            // on interrupt do
-        }
-
-    stage('Sonar') 
-	{
-        env.TAREA =  env.STAGE_NAME
-		stage('SonarQube analysis') 
-		{
-            env.TAREA =  env.STAGE_NAME
-		    // Coresponde a lo que se configuro en tool conffiguration
-			def scannerHome = tool 'Sonar-Scanner';
-
-			withSonarQubeEnv('Sonar-Server') 
-			{ 
-                bat start "${scannerHome}\\bin\\Sonar-Scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
-			}
-		}
-    }
-
+        sonar.call()
 
         run.call()
         rest.call()
