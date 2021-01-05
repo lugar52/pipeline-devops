99a3a55e9c722e1d11533144b6b5c92c41bbfd32
lugar52 <37659063+lugar52@users.noreply.github.com>
2021-01-05 11:43:44 -0300

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -32,9 +32,9 @@ def call(){
                         env.TAREA = env.STAGE_NAME
                 }
 
-                withSonarQubeEnv(installationName: 'Sonar') 
+                withSonarQubeEnv(installationName: 'Sonar-Server') 
                 { // You can override the credential to be used
-                bat 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+                bat './mvnw.cmd org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
                 }
         }
         stage('uploadNexus'){
@@ -42,7 +42,7 @@ def call(){
                         env.TAREA = env.STAGE_NAME
                 }
 
-                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Luis Garrido\\Desktop\\Devops\\ejemplo-maven\\ejemplo-maven\\build\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Luis Garrido\\Desktop\\Devops\\ejemplo-maven\\build\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
         }
 }
 
