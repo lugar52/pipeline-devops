0de923ea9476c829c1038ae3cfffa067d611d5fc
Luis Garrido <garrluis@gmail.com>
2021-01-09 17:35:09 -0300

    sabado

--- vars/stgradle.groovy
+++ vars/stgradle.groovy
@@ -25,33 +25,33 @@ def sonar(){
         }
     }
 }
-// 
-// def nexus(){ 
-//         stage('Nexus') 
-// 	{
-//         figlet env.STAGE_NAME
-//         env.TAREA =  env.STAGE_NAME
-//         nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Luis Garrido\\Desktop\\Devops\\ejemplo-gradle\\build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
-//     }
-// }
-// 
-// 
-// def rest(){ 
-//     stage('Rest') 
-// 	{
-//         figlet env.STAGE_NAME
-//         env.TAREA =  env.STAGE_NAME
-//         "curl -X GET 'http://localhost:8898/rest/mscovid/test?msg=testing'"
-//     }
-// }
-// 
-// def run(){
-//     stage('Run')
-// 	{
-//         figlet env.STAGE_NAME
-//         env.TAREA =  env.STAGE_NAME
-//         "start gradlew bootRun &"
-//     }
-// }
+
+def nexus(){ 
+        stage('Nexus') 
+	{
+        figlet env.STAGE_NAME
+        env.TAREA =  env.STAGE_NAME
+        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Luis Garrido\\Desktop\\Devops\\ejemplo-gradle\\build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+    }
+}
+
+
+def rest(){ 
+    stage('Rest') 
+	{
+        figlet env.STAGE_NAME
+        env.TAREA =  env.STAGE_NAME
+        "curl -X GET 'http://localhost:8898/rest/mscovid/test?msg=testing'"
+    }
+}
+
+def run(){
+    stage('Run')
+	{
+        figlet env.STAGE_NAME
+        env.TAREA =  env.STAGE_NAME
+        "start gradlew bootRun &"
+    }
+}
 // 
 // return this;
