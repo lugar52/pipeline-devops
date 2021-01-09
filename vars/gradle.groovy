6290e6b7a1a87f184daae823120af673c1b47197
Luis Garrido <garrluis@gmail.com>
2021-01-09 16:18:36 -0300

    sabado

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -3,7 +3,7 @@
 	def ejecucion = load 'script.groovy'
 	ejecucion.call()
 */
-import gradle.*
+import gra.*
 
 def call(String miparam){
 
@@ -17,7 +17,7 @@ def call(String miparam){
     str = miparam.split(';');
     if (miparam == ";") {
         println "El valor ingresado es vacio, se procesan todos los stages: " 
-        gradle.build.call()
+        build.call()
 
         sonar.call()
 
