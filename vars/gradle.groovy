b3457ddb130f37055e4c6acce9ed509db9d65f48
Luis Garrido <garrluis@gmail.com>
2021-01-10 17:41:14 -0300

    domingo

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -19,7 +19,6 @@ def call(){
 
 
     def text = "some text to match"
-    def m = text =~ /match/
     
     if (BRANCH_NAME ==~ /develop/ ) {                                                         
         figlet "INTEGRACION CONTINUA"
