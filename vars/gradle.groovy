01b6d84f24c4893dc4d7cd0dad19635463b692f6
Luis Garrido <garrluis@gmail.com>
2021-01-10 17:48:08 -0300

    domingo

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -4,42 +4,25 @@ def call(){
     // def matcher_fea = BRANCH_NAME =~ /feature/
     // def matcher_rel = BRANCH_NAME =~ /release/
 
-    def list = ['buildAndTest','sonar','runJar','rest','nexusCI']
-    // def cdstag = ['downloadNexus','runDownloadedJar','rest','nexusCD']
+    def ci = ['buildAndTest','sonar','runJar','rest','nexusCI']
+    def cd = ['downloadNexus','runDownloadedJar','rest','nexusCD']
 
     if (BRANCH_NAME ==~ /develop/ || BRANCH_NAME ==~ /feature/  ) {                                                         
         figlet "INTEGRACION CONTINUA"
-        list.eachWithIndex { it, i -> 
-            println "Stage a procesar: " + list[i] + ' it: ' + it    
-        // stagesMaven."${it}"()
-        }
-
-        
+        ci.eachWithIndex { it, i -> 
+            println "Stage a procesar: " + ci[i] + ' it: ' + it    
+            cigradle."${it}"()
+        }      
     }
-    else {
-        throw new RuntimeException("Oops, text not found!")
-    }
-   
-    // if (m.find(0)) {                                                         
-    //     
-    //     
-    //     
-    // }
     
- //   if (matcher_dev || matcher_fea) {
-
-
-   // }
-
-
-    // if (matcher_rel) {
-    //     figlet 'ENTREGA CONTINUA'
-    //     
-    //     cdstag.eachWithIndex { it, i -> 
-    //         println "Stage a procesar: " + it    
-    //         cd_gradle."${it}"()
-    //     }
-    // }
+    if (matcher_rel) {
+        figlet 'ENTREGA CONTINUA'
+        
+        cd.eachWithIndex { it, i -> 
+            println "Stage a procesar: " + cd[i] + ' it: ' + it    
+            cdgradle."${it}"()
+        }
+    }
 
 }
 
