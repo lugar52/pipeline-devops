fc1386a3c73a028f5ac777c2aaa7e0c3737691d4
Luis Garrido <garrluis@gmail.com>
2021-01-10 16:52:26 -0300

    domingo

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -9,9 +9,9 @@ def call(){
     
     // `it` is the current element, while `i` is the inde   
 
-    def matcher_dev = BRANCH_NAME =~ /develop/
-    def matcher_fea = BRANCH_NAME =~ /feature/
-    def matcher_rel = BRANCH_NAME =~ /release/
+    // def matcher_dev = BRANCH_NAME =~ /develop/
+    // def matcher_fea = BRANCH_NAME =~ /feature/
+    // def matcher_rel = BRANCH_NAME =~ /release/
     
     
  //   if (matcher_dev || matcher_fea) {
@@ -20,7 +20,7 @@ def call(){
          list.eachWithIndex { it, i -> 
             println "Stage a procesar: "
                
-        }
+            }
    // }
 
 
