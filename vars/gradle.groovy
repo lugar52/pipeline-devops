e9b632bb82729d33e49251fe99c6718b9f3ae7f1
Luis Garrido <garrluis@gmail.com>
2021-01-10 16:54:07 -0300

    domingo

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,17 +1,19 @@
 def call(){
 
-    // def list = ['buildAndTest','sonar','runJar','rest','nexusCI']
+    // def matcher_dev = BRANCH_NAME =~ /develop/
+    // def matcher_fea = BRANCH_NAME =~ /feature/
+    // def matcher_rel = BRANCH_NAME =~ /release/
+
+    def list = ['buildAndTest','sonar','runJar','rest','nexusCI']
     // def cdstag = ['downloadNexus','runDownloadedJar','rest','nexusCD']
 
-    def list = ['compile_code','test_code','jar_code','sonarQube','uploadNexus']
+    // def list = ['compile_code','test_code','jar_code','sonarQube','uploadNexus']
 
     
     
     // `it` is the current element, while `i` is the inde   
 
-    // def matcher_dev = BRANCH_NAME =~ /develop/
-    // def matcher_fea = BRANCH_NAME =~ /feature/
-    // def matcher_rel = BRANCH_NAME =~ /release/
+
     
     
  //   if (matcher_dev || matcher_fea) {
