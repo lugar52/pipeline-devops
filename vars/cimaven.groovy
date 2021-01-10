25f8f58c2093d189ddbc60f5b15439158f487c3a
Luis Garrido <garrluis@gmail.com>
2021-01-10 19:07:13 -0300

    domingo

--- vars/cimaven.groovy
+++ vars/cimaven.groovy
@@ -12,3 +12,32 @@ def buildAndTest(){
     }
 }
 
+def sonar(){
+    stage('sonar') {
+        figlet env.STAGE_NAME
+    }
+}
+
+def runJar(){
+        stage('runJar') {
+        figlet env.STAGE_NAME
+    }
+}
+
+def rest(){
+        stage('rest') {
+        figlet env.STAGE_NAME
+    }
+}
+
+def nexusCI(){
+        stage('nexusCI') {
+        figlet env.STAGE_NAME
+    }
+}
+
+def otro(){
+    stage('otro') {
+        figlet env.STAGE_NAME
+    }
+}
