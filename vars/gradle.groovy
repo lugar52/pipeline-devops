df85dec67739fbe9cf2b39dd888128fc657f8f24
Luis Garrido <garrluis@gmail.com>
2021-01-10 20:20:45 -0300

    domingo

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -9,21 +9,78 @@ def call(){
 
     if (BRANCH_NAME ==~ /develop/ || BRANCH_NAME ==~ /feature/  ) {                                                         
         figlet "INTEGRACION CONTINUA"
-        ci.eachWithIndex { it, i -> 
-            println "Stage a procesar: " + ci[i] + ' it: ' + it    
-            cigradle."${it}"()
+        ci.each {
+            println "Stage a procesar: " ${it} 
+            stage(it){
+                try {
+                    "${it}"()
+                }
+                catch {
+                    error "Error en el Stage: ${it} ${e}" 
+                }
+            }
         }      
     }
     
     if (matcher_rel) {
         figlet 'ENTREGA CONTINUA'
         
-        cd.eachWithIndex { it, i -> 
+        cd.each {
+            stage(it){
+                try {
+                    "${it}"()
+                }
+                catch {
+                    error "Error en el Stage: ${it} ${e}" 
+                }
+            }
+            
             println "Stage a procesar: " + cd[i] + ' it: ' + it    
             cdgradle."${it}"()
         }
     }
 
+}
+
+def buildAndTest(){ 
+    figlet env.STAGE_NAME
+    bat "gradlew clean build"
+}
+
+def nexusCI(){ 
+    figlet env.STAGE_NAME
+    
+    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Luis Garrido\\Desktop\\Devops\\ejemplo-gradle\\build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+}
+
+
+def sonar(){ 
+    figlet env.STAGE_NAME
+    // Coresponde a lo que se configuro en tool conffiguration
+    def scannerHome = tool 'Sonar-Scanner';
+        
+    withSonarQubeEnv('Sonar-Server') 
+    { 
+        bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+    }
+}
+
+def runJar(){
+    figlet env.STAGE_NAME
+    env.TAREA =  env.STAGE_NAME
+    "start gradlew bootRun &"
+}
+
+def rest(){ 
+    figlet env.STAGE_NAME
+    env.TAREA =  env.STAGE_NAME
+    "curl -X GET 'http://localhost:8898/rest/mscovid/test?msg=testing'"
+}
+
+def otro(){
+    stage('otro'){
+
+    }
 }
 
 return this;
