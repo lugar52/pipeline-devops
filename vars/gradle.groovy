034bc4c1c7354f55761ffdfd13f5017aedb5c31f
Luis Garrido <garrluis@gmail.com>
2021-01-10 21:23:12 -0300

    re tarde

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -4,8 +4,8 @@ def call(){
     // def matcher_fea = BRANCH_NAME =~ /feature/
     // def matcher_rel = BRANCH_NAME =~ /release/
 
-    def ci = ['buildAndTest','sonar','runJar','rest','nexusCI', 'otro']
-    def cd = ['downloadNexus','runDownloadedJar','rest','nexusCD']
+    def ci = ['buildAndTest','sonar','runJar','rest','nexusCI', 'otro' ]
+    def cd = ['downloadNexus','runDownloadedJar','rest','nexusCD', 'otro' ]
 
     if (BRANCH_NAME ==~ /develop/ || BRANCH_NAME ==~ /feature/  ) {                                                         
         figlet "INTEGRACION CONTINUA"
@@ -22,7 +22,7 @@ def call(){
         }      
     }
     
-    if (matcher_rel) {
+    if (BRANCH_NAME ==~ /release/ ) {
         figlet 'ENTREGA CONTINUA'
         
         cd.each {
@@ -31,12 +31,10 @@ def call(){
                     "${it}"()
                 }
                 catch(Exception e) {
-                    error "Error en el Stage: ${it} ${e}" 
+                    error "Error en el Stage: " 
                 }
             }
             
-            println "Stage a procesar: " + cd[i] + ' it: ' + it    
-            cdgradle."${it}"()
         }
     }
 
@@ -67,13 +65,11 @@ def sonar(){
 
 def runJar(){
     figlet env.STAGE_NAME
-    env.TAREA =  env.STAGE_NAME
     "start gradlew bootRun &"
 }
 
 def rest(){ 
     figlet env.STAGE_NAME
-    env.TAREA =  env.STAGE_NAME
     "curl -X GET 'http://localhost:8898/rest/mscovid/test?msg=testing'"
 }
 
@@ -81,6 +77,32 @@ def otro(){
     stage('otro'){
         figlet "FIN"
     }
+}
+
+//////////////////////////////////////////
+
+
+// 'downloadNexus','runDownloadedJar','rest','nexusCD'
+
+def downloadNexus(){ 
+    figlet env.STAGE_NAME
+    
+    "curl -X GET -u admin:Vicente$$24 http://localhost:8081/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"
+}
+
+def runDownloadedJar(){
+    figlet env.STAGE_NAME
+    "start gradlew bootRun &"
+}
+
+def rest(){ 
+    figlet env.STAGE_NAME
+    "curl -X GET 'http://localhost:8898/rest/mscovid/test?msg=testing'"
+}
+
+def nexus(){ 
+    figlet env.STAGE_NAME
+    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Luis Garrido\\Desktop\\Devops\\ejemplo-gradle\\build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
 }
 
 return this;
