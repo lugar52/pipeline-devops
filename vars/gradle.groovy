86517da98f828de77fa11e7271a25a418b481665
Luis Garrido <garrluis@gmail.com>
2021-01-10 22:08:11 -0300

    re tarde

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -82,14 +82,17 @@ def otro(){
 
 
 def downloadNexus(){
+    figlet env.STAGE_NAME
     "curl -X GET -u admin:password http://localhost:8081/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"
 }
 
 def runDownloadedJar(){
+    figlet env.STAGE_NAME
     "start gradlew bootRun &"
 }
 
 def nexusCD(){ 
+    figlet env.STAGE_NAME
     nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Luis Garrido\\Desktop\\Devops\\ejemplo-gradle\\build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
 }
 
