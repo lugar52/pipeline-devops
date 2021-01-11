876b81428e92855e6e72dd77dcca862dbaeae2b2
Luis Garrido <garrluis@gmail.com>
2021-01-10 21:28:28 -0300

    re tarde

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -23,11 +23,12 @@ def call(){
     }
     
     if (BRANCH_NAME ==~ /release/ ) {
-        figlet 'ENTREGA CONTINUA'
+        figlet "ENTREGA CONTINUA"
         
         cd.each {
             stage(it){
                 try {
+                    figlet it
                     "${it}"()
                 }
                 catch(Exception e) {
@@ -85,23 +86,19 @@ def otro(){
 // 'downloadNexus','runDownloadedJar','rest','nexusCD'
 
 def downloadNexus(){ 
-    figlet env.STAGE_NAME
     
     "curl -X GET -u admin:Vicente$$24 http://localhost:8081/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"
 }
 
 def runDownloadedJar(){
-    figlet env.STAGE_NAME
     "start gradlew bootRun &"
 }
 
 def rest(){ 
-    figlet env.STAGE_NAME
     "curl -X GET 'http://localhost:8898/rest/mscovid/test?msg=testing'"
 }
 
 def nexus(){ 
-    figlet env.STAGE_NAME
     nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Luis Garrido\\Desktop\\Devops\\ejemplo-gradle\\build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
 }
 
