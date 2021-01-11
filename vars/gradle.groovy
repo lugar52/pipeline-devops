7e9fd8b17087170f8487d205436a4da860904386
Luis Garrido <garrluis@gmail.com>
2021-01-10 21:42:43 -0300

    re tarde

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -87,11 +87,7 @@ def runDownloadedJar(){
     "start gradlew bootRun &"
 }
 
-def rest(){ 
-    "curl -X GET 'http://localhost:8898/rest/mscovid/test?msg=testing'"
-}
-
-def nexus(){ 
+def nexusCD(){ 
     nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Luis Garrido\\Desktop\\Devops\\ejemplo-gradle\\build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
 }
 
